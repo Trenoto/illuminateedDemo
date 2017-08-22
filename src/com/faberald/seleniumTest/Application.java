6817b0eb33fb2b6a595c71d192d13013334e3ae3
Hongtao Liu <faberaldliu@gmail.com>
2017-08-22 00:23:47 -0700

    update

--- src/com/faberald/seleniumTest/Application.java
+++ src/com/faberald/seleniumTest/Application.java
@@ -1,16 +1,25 @@
 package com.faberald.seleniumTest;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class Application {
-    public static void main(String[] args)  throws InterruptedException{
+    public static void main(String[] args)  throws Exception{
         AutoTest test1 = new AutoTest("https://demo.illuminateed.com");
-        String[] hypertexts = new String[]{"release nos", "Back to School Illuminations Newsletter",
+        String[] hypertexts = new String[]{"Forgot your password?","www.illuminateed.com", "release notes", "Back to School Illuminations Newsletter",
                                         "Webinars", "8/17", "8/31", "8/17,8/24,8/31,9/7,9/14,9/21,9/28", "8/24",
                                         "9/7,9/20/10/2,10/20", "More", "8/15-8/17", "9/13", "9/19-9/20",
                                         "9/19", "10/23-10/24",  "more", "11/14", "11/6-11/7", "2/1-2/2"};
 
+        PrintStream out = new PrintStream(new FileOutputStream("./output.txt"));
+        System.setOut(out);
+        System.out.println("------------------------Test Begin------------------------");
+        test1.staticWebTest();
         test1.loginTest("testuser","testPassword");
+        test1.testGoogleLogin();
         test1.testSocialBtn();
         test1.testhyperLink(hypertexts);
-
+        test1.endTest();
+        System.out.println("------------------------ Test End ------------------------");
     }
 }
--- src/com/faberald/seleniumTest/AutoTest.java
+++ src/com/faberald/seleniumTest/AutoTest.java
@@ -1,12 +1,12 @@
 package com.faberald.seleniumTest;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.NoSuchElementException;
 
+import java.io.File;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -14,60 +14,124 @@ import java.util.Set;
 public class AutoTest {
     private WebDriver driver;
     private String mainPage;
+    private String mainHandler;
 
     public AutoTest(String url) {
         driver = new ChromeDriver();
         driver.get(url);
         this.mainPage = driver.getCurrentUrl();
+        this.mainHandler = driver.getWindowHandle();
     }
 
-    public void loginTest(String username, String password) throws InterruptedException{
+    /**
+     * Test static web contents, to see if there is title/header/footer
+     */
+    public void staticWebTest(){
         System.out.println("===========================================");
-        System.out.println("Login Test");
-        driver.findElement(By.id("username")).sendKeys(username);
-        driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.id("button_next")).click();
-        Thread.sleep(1000);
+        System.out.println("Static Web-Content test\n");
+        try {
+            System.out.println("Title: " + driver.getTitle());
+        }catch(Exception e){
+            System.out.println("No Title");
+        }
 
         try{
-            System.out.println(driver.switchTo().alert().toString());
-        }catch (NoAlertPresentException ex){
-            System.out.println("No pop-up Alter Window Present");
+            System.out.println("Header: "+ driver.findElement(By.tagName("h1")).getText().toString() );
+        }catch(NoSuchElementException e){
+            System.out.println("No h1 header");
+        }
+
+        try{
+            System.out.println("Footer: "+ driver.findElement(By.id("footer")).findElement(By.id("footer-copyright")).getText().toString());
+        }catch(NoSuchElementException e){
+            System.out.println("No footer");
         }
 
         System.out.println("===========================================\n\n");
     }
 
+    /**
+     * *
+     * Simple login test, test input text box's function
+     * It will redirect to a new page when login success
+     * it should display an error message when login fail
+     * @param  username a string
+     * @param  password a string
+     * *
+     */
+    public void loginTest(String username, String password) throws InterruptedException{
+        System.out.println("===========================================");
+        System.out.println("Login Test\n");
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("button_next")).click();
+        Thread.sleep(1000);
+        if (driver.getCurrentUrl().equals(mainPage))
+            System.out.println("Login Failed " + driver.findElement(By.xpath("//*[@id='invalid_login']")).getText().toString());
+        else
+            System.out.println("Login Succeed");
+
+        try{
+            System.out.println(driver.switchTo().alert().toString());
+        }catch (NoAlertPresentException ex){
+            System.out.println("*No pop-up Alter Window Present*");
+        }
+
+        System.out.println("===========================================\n\n");
+    }
+
+    /**
+     * *
+     * Test back function
+     * *
+     */
     public void backToLastPage(){
+        System.out.println("Back to prev page");
         driver.navigate().back();
     }
 
-    public void openHyperLink(String hyperText){
+    /**
+     * *
+     * Test a hyperlink
+     * It will display the exception and error message when element is not found
+     * @param  hyperText(String) a link text that appears in HTML file
+     * @return      void no return
+     * *
+     */
+    public void openHyperLink(String hyperText) throws InterruptedException{
         System.out.println("******************************");
         System.out.println("HyperLink text : " + hyperText);
         try {
             driver.findElement(new By.ByLinkText(hyperText)).click();
-        }catch (org.openqa.selenium.NoSuchElementException ex){
+            System.out.println("Redirect to " + driver.getCurrentUrl());
+        }catch (NoSuchElementException ex){
             System.out.println(ex + "Can't locate " + hyperText);
         }
-
-        System.out.println("Redirect to " + driver.getCurrentUrl());
+        Thread.sleep(1000);
+        backToLastPage();
         System.out.println("******************************\n");
 
     }
 
-    public void testhyperLink(String[] hyperTexts) throws InterruptedException{
+    /**
+     * *
+     * Test all hyperlink
+     * It will show a error message when browser can't back to prev page in 1 click
+     * @param  hyperTexts(String[]) a array contains a set of hyper texts
+     * *
+     */
+    public void testhyperLink(String[] hyperTexts) throws Exception{
         System.out.println("===========================================");
-        System.out.println("hyper Link Test");
+        System.out.println("hyper Link Test\n");
         for (String hyperText: hyperTexts){
             openHyperLink(hyperText);
             Thread.sleep(1000);
-            System.out.println("Back to prev page");
-            backToLastPage();
             try {
                 Assert.assertTrue(driver.getCurrentUrl().equals(mainPage));
             }catch(AssertionError ex){
                 System.out.print("Minor Exception Occurs: Can't back to prev page in 1 click\n");
+                File pic = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+                FileUtils.copyFile(pic,new File("./screenshot.png"));
                 driver.get(mainPage);
                 Thread.sleep(1000);
             }
@@ -78,10 +142,14 @@ public class AutoTest {
 
     }
 
+    /**
+     * *
+     * Test all Buttons
+     * *
+     */
     public void testSocialBtn() throws InterruptedException{
         System.out.println("===========================================");
-        System.out.println("Button Test");
-        String mainHandler = driver.getWindowHandle();
+        System.out.println("Button Test\n");
 
         List<WebElement> btns = driver.findElement(By.className("social_media")).findElements(By.tagName("a"));
 
@@ -98,9 +166,42 @@ public class AutoTest {
             System.out.println("Redirect to : " + driver.getTitle() + " URL is : " + driver.getCurrentUrl());
             Thread.sleep(100);
         }
-
+        System.out.println("===========================================\n\n");
         driver.switchTo().window(mainHandler);
 
-        System.out.println("===========================================\n\n");
     }
+
+    /**
+     * *
+     * Test google Login Page
+     * *
+     */
+    public void testGoogleLogin() throws InterruptedException{
+        System.out.println("===========================================");
+        System.out.println("Google Login Test\n");
+        driver.findElement(By.xpath("//*[@id='ied-login-logo']")).click();
+        Thread.sleep(1000);
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> iterator = handles.iterator();
+
+        iterator.next();
+        while(iterator.hasNext()){
+            driver.switchTo().window(iterator.next());
+            System.out.println("Redirect to : " + driver.getTitle() + " URL is : " + driver.getCurrentUrl());
+            Thread.sleep(500);
+        }
+
+        System.out.println("===========================================\n\n");
+        driver.switchTo().window(mainHandler);
+    }
+
+    /**
+     * *
+     * End Test and close the browser window
+     * *
+     */
+    public void endTest(){
+        driver.close();
+    }
+
 }
